244e2e0a1cb3f30ac7f0b7a132e0d81119773bdb
Alexis LOURS <alexislours@protonmail.com>
2023-01-18 17:21:32 +0100

    feat: style closed navbar

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,9 +1,20 @@
 <script setup>
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
+import { computed } from "@vue/reactivity";
+import Burger from '../assets/icons/burger.svg'
+import Home from '../assets/icons/home.svg'
+import Auctions from '../assets/icons/auctions.svg'
+import Map from '../assets/icons/map.svg'
+import MyBids from '../assets/icons/my-bids.svg'
+import Account from '../assets/icons/account.svg'
 
 const store = useStore();
 const router = useRouter();
+
+const currentRoute = computed(() => router.currentRoute.value.name)
+
+console.log(currentRoute)
 
 const logout = () => {
   store.dispatch("auth/logout");
@@ -11,41 +22,41 @@ const logout = () => {
 };
 </script>
 <template>
-  <nav class="shadow bg-slate-900">
-    <div
-      class="flex items-center justify-center p-6 mx-autocapitalize text-gray-300"
-    >
-      <RouterLink
-        :to="{ name: 'home' }"
-        class="text-gray-200 border-blue-500 mx-1.5 sm:mx-6"
-      >
-        Home
-      </RouterLink>
-      <RouterLink
-        :to="{ name: 'bids' }"
-        class="text-gray-200 border-blue-500 mx-1.5 sm:mx-6"
-      >
-        Enchères
-      </RouterLink>
-      <RouterLink
-        :to="{ name: 'cart' }"
-        class="text-gray-200 border-blue-500 mx-1.5 sm:mx-6"
-      >
-        Mes enchères
-      </RouterLink>
-      <RouterLink
-        :to="{ name: 'login' }"
-        class="text-gray-200 border-blue-500 mx-1.5 sm:mx-6"
-      >
-        Login
-      </RouterLink>
-      
-      <a
-        class="text-gray-200 border-blue-500 mx-1.5 sm:mx-6 cursor-pointer"
-        @click="logout"
-      >
-        Logout
-      </a>
+  <nav class="shadow bg-slate-900 w-20 hidden md:block h-screen fixed">
+    <div class="flex flex-col h-screen items-center justify-start mx-autocapitalize text-gray-300">
+      <div class="h-20 w-20 cursor-pointer flex items-center justify-center">
+        <img :src="Burger" class="hover:scale-105 transition-all duration-200" />
+      </div>
+      <div class="px-2 w-full">
+        <div class="h-[1px] w-full bg-white"></div>
+      </div>
+      <div class="flex-1">
+        <div class="h-20 w-20 flex justify-center items-center">
+          <RouterLink :to="{ name: 'home' }" :class="`h-16 w-16 transition-all duration-200 rounded-lg cursor-pointer flex items-center justify-center ${currentRoute === 'home' ? 'bg-slate-700' : 'hover:bg-slate-800'}`">
+            <img :src="Home" class="hover:scale-105 transition-all duration-200" />
+          </RouterLink>
+        </div>
+        <div class="h-20 w-20 flex justify-center items-center">
+          <RouterLink :to="{ name: 'bids' }" :class="`h-16 w-16 transition-all duration-200 rounded-lg cursor-pointer flex items-center justify-center ${currentRoute === 'bids' ? 'bg-slate-700' : 'hover:bg-slate-800'}`">
+            <img :src="Auctions" class="hover:scale-105 transition-all duration-200" />
+          </RouterLink>
+        </div>
+        <div class="h-20 w-20 flex justify-center items-center">
+          <RouterLink :to="{ name: 'map' }" :class="`h-16 w-16 transition-all duration-200 rounded-lg cursor-pointer flex items-center justify-center ${currentRoute === 'map' ? 'bg-slate-700' : 'hover:bg-slate-800'}`">
+            <img :src="Map" class="hover:scale-105 transition-all duration-200" />
+          </RouterLink>
+        </div>
+        <div class="h-20 w-20 flex justify-center items-center">
+          <RouterLink :to="{ name: 'cart' }" :class="`h-16 w-16 transition-all duration-200 rounded-lg cursor-pointer flex items-center justify-center ${currentRoute === 'cart' ? 'bg-slate-700' : 'hover:bg-slate-800'}`">
+            <img :src="MyBids" class="hover:scale-105 transition-all duration-200" />
+          </RouterLink>
+        </div>
+      </div>
+      <div class="h-20 w-20 flex justify-center items-center bg-slate-700">
+        <RouterLink :to="{ name: 'login' }" class="h-16 w-16 cursor-pointer flex items-center justify-center">
+          <img :src="Account" class="hover:scale-105 transition-all duration-200" />
+        </RouterLink>
+      </div>
     </div>
   </nav>
 </template>
---
+++ client/src/views/Map.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Map from '../components/Map.vue';
+</script>
+
+<template>
+    <h1>Carte des enchères</h1>
+    <Map />
+</template>
+
+<style>
+
+</style>
