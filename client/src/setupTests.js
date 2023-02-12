f4c70cce02a3269d5821eee4c9719ea9e812ef1f
Prado <mahery.rsh@gmail.com>
2023-02-12 23:40:40 +0100

    fix: ci tests failures

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,6 +1,8 @@
 import "@testing-library/jest-dom";
 import server from "./mocks/server";
+import { cleanup } from "@testing-library/vue";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+afterEach(cleanup);
--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vitest/config'
 import Vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [Vue()],
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        maxThreads: 1,
-        minThreads: 1,
-        setupFiles: 'src/setupTests.js'
-    },
-})
+  plugins: [Vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    threads: false,
+    setupFiles: "src/setupTests.js",
+  },
+});
