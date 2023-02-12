ab707c7adc6f6021075fa937a02552a19594460b
Sadik Raida <sadik.raida@gmail.com>
2023-02-12 22:40:01 +0100

    feat: event fixtures

--- server/api/src/DataFixtures/EventFixtures.php
+++ server/api/src/DataFixtures/EventFixtures.php
@@ -5,39 +5,73 @@ namespace App\DataFixtures;
 use App\Entity\Bid;
 use App\Entity\Animal;
 use App\Entity\User;
+use DateTimeImmutable;
 use App\Entity\Event;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 class EventFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $debut= mt_rand(1674505348,1674764548);
-        $dateDebut = new \DateTimeImmutable();
-        $dateDebut = $dateDebut->setTimestamp($debut);  
 
-        $creator = $manager->getRepository(User::class)->findOneBy(['name' => 'client0']);
-        for($i = 0; $i < 25; $i++){
+        $faker = Factory::create('en_US');
+
+        $eventInfo = array (
+
+        );
+
+        $eventDescription = [
+            "Vente secrète de Lion" =>  "Vente illégale de lionceaux.",
+         "Vente secrète de Tigre de siberie" => "Vente secrète de tigres de Siberie.",
+          "Vente secrète de Lémurien" => "Vente de Lémuriens sous le manteau.",
+          "Vente secrète de Kangourou" => "Vente de Kangourou.",
+           "Vente secrète de Koala" => "Achat de Koala adultes.",
+           "Vente secrète de Ornithorynque" => "Vente de bébés ornithorynque.",
+           "Vente secrète de Panda roux" => "Marché noir de panda roux.",
+           "Vente secrète de Wombat" => "Achat de wombat de combat.",
+           "Vente secrète de Paresseux" => "Vente illégale de paresseux nains.",
+           "Vente secrète de Tamanoir" => "Vente de tamanoir.", 
+           "Vente secrète de Hippopotame" => "Vente d'Hippopotames adultes", 
+           "Vente secrète de Girafe" => "Vente de girafes à couper le souffle."
+        ];
+
+        $eventName = ["Vente secrète de Lion", "Vente secrète de Tigre de siberie", "Vente secrète de Lémurien", "Vente secrète de Kangourou", "Vente secrète de Koala", "Vente secrète de Ornithorynque", "Vente secrète de Panda roux", "Vente secrète de Wombat", "Vente secrète de Paresseux", "Vente secrète de Tamanoir", "Vente secrète de Hippopotame", "Vente secrète de Girafe"];
+
+        
+ 
+
+        $creators = $manager->getRepository(User::class)->findAll();
+        $seller =[];
+        for ($i = 0; $i < count($creators); $i++) {
+            if (in_array("ROLE_SELLER", $creators[$i]->getRoles())) {
+                array_push($seller, $creators[$i]);
+            }
+        }
+
+        for($i = 0; $i < count($eventName); $i++){
             
-            $debut= mt_rand(1674505348,1674764548);
-            $dateDebut = new \DateTimeImmutable();
-            $dateDebut = $dateDebut->setTimestamp($debut);  
-
-            $event = new Event();
+            $dateDebut = $faker->dateTimeBetween('-2 week', '+4 week');  
+            $dateDebut = DateTimeImmutable::createFromMutable($dateDebut);
+            
+            
+                $event = new Event();
             $event
-                ->setName('Bakalaye vend des Marsupilami'.$i)
-                ->setSlug('slug'.$i)
-                ->setDescription('Ici ça bicrave du marsu'.$i)
-                ->setPrice(100)
-                ->setAddress($i.' rue de la Jugnle')
+                ->setName($eventName[$i])
+                ->setSlug(str_replace(' ','-',$eventName[$i]))
+                ->setDescription($eventDescription[$eventName[$i]])
+                ->setPrice($faker->numberBetween(200, 3000))
+                ->setAddress("24 boulevard du double rotor, 75013 Paris")
                 ->setCapacity(16)
                 ->setDate($dateDebut)
-                ->setCreator($creator)
+                ->setCreator($faker->randomElement($seller))
             ;
-
             $manager->persist($event);
+            
+
+            
         }
         $manager->flush();
     }
