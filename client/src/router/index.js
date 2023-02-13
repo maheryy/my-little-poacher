a089dce0e98d2c1d97325361bd0337c275072f7b
Sadik Raida <sadik.raida@gmail.com>
2023-02-13 21:51:30 +0100

    feat: update routing

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import publicRoutes from "./public";
 import protectedRoutes from "./protected";
 import protectedProRoutes from "./pro";
+import protectedAdminRoutes from "./admin";
 
 const routes = [
   ...publicRoutes,
   ...protectedRoutes,
   ...protectedProRoutes,
+  ...protectedAdminRoutes,
   {
     path: "/:pathMatch(.*)",
     name: "not-found",
